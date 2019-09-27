8d1a675f4802f0c7a887780d3765e19fcbef6447
Abyanware <abyanware@abyanware.local>
2019-09-27 16:47:34 +0800

    2 - modify codebase into es6 syntax; install babel; add .babelrc; edit package.json start script to use babel-node instead of node; edit code syntax into es6 syntax

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send(JSON.stringify({
+        "result": "API root"
+    }));
+});
+
+export default router;
