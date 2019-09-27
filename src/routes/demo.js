ee44549ea952918d1176bdeabcf8a8def31371de
Abyanware <abyanware@abyanware.local>
2019-09-27 18:55:20 +0800

    3 - implement run dev script; install npm-run-all, nodemon; add nodemon.json

--- src/routes/demo.js
+++ src/routes/demo.js
@@ -1,10 +1,15 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/demo', function (req, res, next) {
-    res.send('respond with a resource');
+
+    const data = {
+        'message': 'API demo is working'
+    };
+
+    res.send(JSON.stringify(data, null, 2));
 });
 
 export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.send(JSON.stringify({
-        "result": "API root"
+        'result': 'API root'
     }));
 });
 
