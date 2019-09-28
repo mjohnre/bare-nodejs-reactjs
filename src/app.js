24166864efd7224d1b8c2c5da3fc3ed46cef8aad
Abyanware <abyanware@abyanware.local>
2019-09-28 09:57:20 +0800

    6 - implement logger; install winston; add logs in .gitignore; add winston config; add routes error handler

--- src/app.js
+++ src/app.js
@@ -1,20 +1,55 @@
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import morgan from 'morgan';
+import logger from "./module/winston";
 
 import indexRouter from './routes';
 import demoRouter from './routes/demo';
 
 const app = express();
 
-app.use(logger('dev'));
+//express middleware
+//logger
+app.use(morgan('combined', {stream: logger.stream}));
+//parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//serve api
 app.use('/api', indexRouter);
 app.use('/api', demoRouter);
+
+//serve static view
+app.use(express.static(path.join(__dirname, 'public')));
+
+//error handler
+app.use(function (req, res, next) {
+    //error 404
+    // set locals, only providing error in development
+    const err = new Error('Not found');
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // add this line to include winston logging
+    logger.error(`${err.status || 404} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+    // render the error page
+    res.status(err.status || 404);
+    res.send('Not found');
+});
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // add this line to include winston logging
+    logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+    // render the error page
+    res.status(err.status || 500);
+    res.send('error');
+});
 
 export default app;
